36d862705766aa8cc39f9007a721e02cc3333de2
Markus <markus.reynoso.c@gmail.com>
2024-10-22 20:58:06 +0800

    Added landing page

---
+++ app.py
@@ -0,0 +1,31 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+introParagraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at pharetra ante. Vestibulum ut sapien id nibh efficitur pulvinar id in leo. Nullam aliquet, velit at fermentum dictum, neque massa vehicula velit, ac scelerisque nunc mi quis eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris finibus volutpat congue. Cras ac gravida dolor. Praesent sed accumsan orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut porttitor nibh sem, vitae varius orci efficitur eget. Proin tincidunt, nisi et molestie congue, nisi nibh dignissim risus, in malesuada erat massa eu dolor. Vestibulum a quam at libero scelerisque tempus.'
+
+app = Dash(__name__)
+app.layout = html.Div(children=[
+    html.H1(children=[
+        'An Integrated View of',
+        html.Br(),
+        'Real Estate Pricing',
+        html.Br(),
+        'and ',
+        html.Span('Seismic Events', style={'color': '#ff8484'})
+    ]),
+
+    html.Div(children=[
+        html.P(introParagraph)
+    ]),
+
+    html.Div(id='spacer'),
+
+    html.Div(
+        html.H2('Earthquakes')
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
