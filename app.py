7cf6cb84df4e4d58a3c419a509c9e18f3c9f619c
Markus <markus.reynoso.c@gmail.com>
2024-10-25 17:53:39 +0800

    Added Earthquake divs, temporary graphs, and regions in the dropdown

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 introParagraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at pharetra ante. Vestibulum ut sapien id nibh efficitur pulvinar id in leo. Nullam aliquet, velit at fermentum dictum, neque massa vehicula velit, ac scelerisque nunc mi quis eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris finibus volutpat congue. Cras ac gravida dolor. Praesent sed accumsan orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut porttitor nibh sem, vitae varius orci efficitur eget. Proin tincidunt, nisi et molestie congue, nisi nibh dignissim risus, in malesuada erat massa eu dolor. Vestibulum a quam at libero scelerisque tempus.'
 
 app = Dash(__name__)
+
 app.layout = html.Div(children=[
     html.H1(children=[
         'An Integrated View of',
@@ -26,35 +27,76 @@ app.layout = html.Div(children=[
         html.H2('Earthquakes')
     ),
 
+    dcc.Dropdown(
+        id='quakesDropdown',
+        style={
+            'font-family': 'Arial, sans-serif',
+            'font-size': '12px',
+            'color': '#022f40'
+        },
+        options=[
+            {'label': 'Region I - Ilocos Region', 'value': 'Region I'},
+            {'label': 'Region II - Cagayan Valley', 'value': 'Region II'},
+            {'label': 'Region III - Central Luzon', 'value': 'Region III'},
+            {'label': 'Region IV-A - CALABARZON', 'value': 'Region IV-A'},
+            {'label': 'Region IV-B MIMAROPA', 'value': 'Region IV-B'},
+            {'label': 'Region V - Bicol Region', 'value': 'Region V'},
+            {'label': 'Region VI - Western Visayas', 'value': 'Region VI'},
+            {'label': 'Region VII - Central Visayas', 'value': 'Region VII'},
+            {'label': 'Region VIII - Eastern Visayas', 'value': 'Region VIII'},
+            {'label': 'Region IX - Zamboanga Peninsula', 'value': 'Region IX'},
+            {'label': 'Region X - Northern Mindanao', 'value': 'Region X'},
+            {'label': 'Region XI - Davao Region', 'value': 'Region XI'},
+            {'label': 'Region XII - SOCCSKSARGEN', 'value': 'Region XII'},
+            {'label': 'Region XIII - Caraga', 'value': 'Region XIII'},
+            {'label': 'NCR - National Capital Region', 'value': 'NCR'},
+            {'label': 'CAR - Cordillera Administrative Region', 'value': 'CAR'},
+            {'label': 'BARMM - Bangsamoro Autonomous Region in Muslim Mindanao', 'value': 'BARMM'},
+            {'label': 'Region XVIII', 'value': 'Region XVIII'}
+        ],
+        value='Overall'
+    ),
+
     html.Div(
-        style={'background-color': 'red', 'height': '700px', 'display': 'flex'},
+        style={'background-color': 'red', 'height': '900px', 'display': 'flex', 'flex-direction': 'column'},  # Flex column layout
         children=[
             html.Div(
                 style={'background-color': 'blue',
-                       'width': '30%'}
+                       'height': '45%',
+                       'display': 'flex',
+                       'flex-direction': 'row'},
+                children=[
+                    html.Div(
+                        style={'background-color': 'yellow',
+                               'width': '30%',
+                               'display': 'flex',
+                               'justify-content': 'center',
+                               'align-items': 'center'},
+                        children=[
+                            dcc.Graph(
+                                id='quakePie',
+                                style={'width': '100%',
+                                       'height': '100%'}
+                            )
+                        ]
+                    ),
+
+                    dcc.Graph(
+                        id='quakeHist',
+                        style={'width': '100%',
+                               'height': '100%'}
+                    )
+                ]
             ),
             html.Div(
-                style={'background-color': 'green',
-                       'width': '70%'}
+                style={'background-color': 'green', 'height': '55%'}
             )
         ]
     ),
 
-    dcc.Dropdown(
-        id='quakesDropdown'
-    ),
-
     html.Br(),
     html.Br(),
     html.Br(),
-
-    html.Center(
-        dcc.Graph(
-            id='graph1',
-            className='wideGraph'
-        )
-    )
 ])
-
 if __name__ == '__main__':
     app.run(debug=True)
